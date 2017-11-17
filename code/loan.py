c9e00cad5c1399246417fd7b4cb30f2f1b11f441
arturomf94 <arturomf94@gmail.com>
2017-11-17 20:22:04 +0000

    Add grade in loan class

--- code/loan.py
+++ code/loan.py
@@ -1,7 +1,8 @@
 
 class loan()
 
-	def __init__(self, amount = 0, length = 0, frequency = 0):
+	def __init__(self, amount = 0, length = 0, frequency = '', grade = ''):
 		self.amount = amount
 		self.length = length
 		self.frequency = frequency
+		self.grade = grade
