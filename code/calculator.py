4e426442e14391db1fb1e8fbe1dda9c74d3412a1
arturomf94 <arturomf94@gmail.com>
2017-11-17 17:14:17 +0000

    Add a filter for loan frequencies

--- code/calculator.py
+++ code/calculator.py
@@ -1,4 +1,5 @@
 from misc import portfolio
+import pprint
 
 # DECLARE INPUTS:
 
@@ -13,33 +14,50 @@ accepted_grades = {
 	'F':0
 }
 
-# Accepted Loan Length
-
-accepted_length = {
-	'6':1,
-	'12':0,
-	'18':0
-}
-
-# FILTER DATA:
-
-filtered_data = []
-
 filtered_grades = [
 	grade 
 	for grade in accepted_grades 
 	if accepted_grades[grade] == 1
 ]
 
+# Accepted Loan Length
+
+accepted_lengths = {
+	'6':1,
+	'12':0,
+	'18':0
+}
+
 filtered_lengths = [
 	length
-	for length in accepted_length
-	if accepted_length[length] == 1
+	for length in accepted_lengths
+	if accepted_lengths[length] == 1
 ]
+
+# Accepted Loan Frequency
+
+accepted_frequencies = {
+	'Semanal':0,
+	'Catorcenal':0,
+	'Mensual':1
+}
+
+filtered_frequencies = [
+	frequency
+	for frequency in accepted_frequencies
+	if accepted_frequencies[frequency] == 1
+]
+
+# FILTER DATA:
+
+filtered_data = []
 
 filtered_data = [
 	row
 	for row in portfolio
 	if row[4] in filtered_grades
 	if row[2] in filtered_lengths
+	if row[3] in filtered_frequencies
 ]
+
+pprint.pprint(filtered_data)
