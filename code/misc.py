7ad19dc29613f94d684e2b94eb008a9085fb1150
arturomf94 <arturomf94@gmail.com>
2017-11-23 16:35:15 +0000

    Irrelevant change commited

--- code/misc.py
+++ code/misc.py
@@ -7,6 +7,3 @@ f = open('../portfolio.csv')
 with open('../portfolio.csv', 'rb') as portfolio_csv:
 	portfolio_reader = csv.reader(portfolio_csv, delimiter=',')
 	portfolio = list(portfolio_reader)
-
-# Clean Data
-
