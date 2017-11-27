98a620066ec59d36420dca56375a4c91afd90d22
arturomf94 <arturomf94@gmail.com>
2017-11-27 16:28:29 +0000

    Add Portfolio class

---
+++ code/portfolio.py
@@ -0,0 +1,8 @@
+from misc import portfolio
+
+class Portfolio:
+
+	def __init_(self, accepted_lengths = {}, accepted_frequencies = {}, accepted_grades = {})
+		self.accepted_lengths = accepted_lengths
+		self.accepted_frequencies = accepted_frequencies
+		self.accepted_grades = accepted_grades
