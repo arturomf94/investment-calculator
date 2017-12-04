51f11da112f59a97a8404b084db45656c9a32723
arturomf94 <arturomf94@gmail.com>
2017-12-04 22:14:37 +0000

    Include sample plot for the returned capital

--- code/calculator.py
+++ code/calculator.py
@@ -1,12 +1,14 @@
 import portfolio as portfolio_module
 import pprint
 import loan as loan_module
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
 
 # DECLARE INPUTS:
 
 # Initial Investment Amount
 
-investment_amount = 1000
+investment_amount = 10000
 
 # Accepted Grades
 
@@ -22,8 +24,8 @@ accepted_grades = {
 # Accepted Loan Length
 
 accepted_lengths = {
-	'6':1,
-	'12':1,
+	'6':0,
+	'12':0,
 	'18':1
 }
 
@@ -43,19 +45,46 @@ portfolio_data = portfolio.get_data()
 
 portfolio_summary = portfolio.get_summary(investment_amount = investment_amount)
 
+# Extract Payments, Capital, Interest and VAT
+
+payment = []
+capital = []
+interest = []
+vat = []
+mes = []
+
+for i in range(0,18):
+	payment.append(portfolio_summary[i][1])
+	capital.append(portfolio_summary[i][2])
+	interest.append(portfolio_summary[i][3])
+	vat.append(portfolio_summary[i][4])
+	mes.append(i + 1)
+
+
+# Plot Capital
+
+plt.bar(mes, capital, align='center', alpha=0.5)
+plt.xlabel('Mes')
+plt.xticks(mes,mes)
+plt.ylabel('$')
+plt.title('Capital')
+
+plt.show()
+
+
 # # TEST
 # print('PORTFOLIO')
 # pprint.pprint(portfolio_data)
-print('MONTHLY CASH FLOW')
-pprint.pprint(portfolio_summary)
-print('CAPITAL RETURNED')
-capital_returned_list = []
-for i in range(0,18):
-	capital_returned_list.append(portfolio_summary[i][2])
-print(capital_returned_list)
-print(sum(capital_returned_list))
+# print('MONTHLY CASH FLOW')
+# pprint.pprint(portfolio_summary)
+# print('CAPITAL RETURNED')
+# capital_returned_list = []
+# for i in range(0,18):
+# 	capital_returned_list.append(portfolio_summary[i][2])
+# print(capital_returned_list)
+# print(sum(capital_returned_list))
 
-test_loan = loan_module.Loan(portfolio_data[1][1], portfolio_data[1][2], portfolio_data[1][3], portfolio_data[1][4])
+# test_loan = loan_module.Loan(portfolio_data[1][1], portfolio_data[1][2], portfolio_data[1][3], portfolio_data[1][4])
 
 # print('### TEST LOAN ###')
 # print('# CASH FLOW #')
